6fe8098d0f4c38e3351fa3a31d40cd2209291e45
Remy Goldschmidt <taktoa@gmail.com>
2016-02-21 12:52:54 -0600

    Added initial set of imports to Pipes/Parsers.hs

--- library/Pipes/Parsers.hs
+++ library/Pipes/Parsers.hs
@@ -1,2 +1,12 @@
 module Pipes.Parsers where
 
+import qualified Data.Text               as Text
+
+import qualified Data.ByteString         as BS
+
+import qualified Pipes                   as Pipes
+import qualified Pipes.Parse             as Pipes
+
+import qualified Text.Parser.Char        as Parsers
+import qualified Text.Parser.Combinators as Parsers
+import qualified Text.Parser.Token       as Parsers
