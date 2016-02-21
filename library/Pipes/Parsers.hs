83a0d399eb4cbe88f1dc97bcf80f4599f8f8a3fb
Remy Goldschmidt <taktoa@gmail.com>
2016-02-21 12:57:27 -0600

    Save before renaming to pipes-trifecta

--- library/Pipes/Parsers.hs
+++ library/Pipes/Parsers.hs
@@ -4,9 +4,17 @@ import qualified Data.Text               as Text
 
 import qualified Data.ByteString         as BS
 
-import qualified Pipes                   as Pipes
 import qualified Pipes.Parse             as Pipes
+import qualified Pipes.Prelude           as Pipes
 
 import qualified Text.Parser.Char        as Parsers
 import qualified Text.Parser.Combinators as Parsers
 import qualified Text.Parser.Token       as Parsers
+
+-- try   :: m a -> m a
+-- (<?>) :: m a -> String -> m a
+-- skipMany :: m a -> m ()
+-- skipSome :: m a -> m ()
+-- unexpected :: String -> m a
+-- eof :: m ()
+-- notFollowedBy :: Show a => m a -> m ()
